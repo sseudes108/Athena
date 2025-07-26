1d33e6b61b9d2777ddd885bd486bf765906c6180
Eudes <eudes.jss92@gmail.com>
2025-07-26 00:34:38 -0300

    Atualizar variaveis com st.session_state. Atualizar chamada dos graficos

--- View/graficos.py
+++ View/graficos.py
@@ -2,28 +2,28 @@ import plotly.graph_objects as go
 import streamlit as st
 
 
-def draw_grafico_demanda_capacidade(df_derivacao, color_der, df_producao, color_cap, range, key):
+def draw_grafico_demanda_capacidade(key):
     fig = go.Figure()
     
     fig.add_scatter(
-        x=df_derivacao['horario'],
-        y=df_derivacao['quantidade'],
+        x=st.session_state.df_derivacao['horario'],
+        y=st.session_state.df_derivacao['quantidade'],
         mode='lines+markers',
         name='Derivação',
         line=dict(
-            color=f'{color_der}',
+            color=f'skyblue',
             width=2,
             shape='hv'
         )
     )
     
     fig.add_scatter(
-        x=df_producao['horario'],
-        y=df_producao['quantidade'],
+        x=st.session_state.df_producao['horario'],
+        y=st.session_state.df_producao['quantidade'],
         mode='lines+markers',
         name='Produção',
         line=dict(
-            color=f'{color_cap}',
+            color=f'violet',
             width=2,
             shape='hv'
         )
@@ -31,23 +31,23 @@ def draw_grafico_demanda_capacidade(df_derivacao, color_der, df_producao, color_
     
     fig.update_layout(
         xaxis=dict(
-            range=range,
+            range=st.session_state.time_range,
             tickangle=45
         )
     )
         
     st.plotly_chart(fig, key=key)
 
-def draw_grafico(dataframe, color, range, key):
+def draw_grafico(key):
     fig = go.Figure()
     
     fig.add_scatter(
-        x=dataframe['horario'],
-        y=dataframe['quantidade'],
+        x=st.session_state.df_acumulo['horario'],
+        y=st.session_state.df_acumulo['quantidade'],
         mode='lines+markers',
         name='Acumulo',
         line=dict(
-            color=f'{color}',
+            color=f'salmon',
             width=2,
             shape='hv'
         )
@@ -61,7 +61,7 @@ def draw_grafico(dataframe, color, range, key):
     
     fig.update_layout(
         xaxis=dict(
-            range=range,
+            range=st.session_state.time_range,
             tickangle=45
         )
     )
--- app.py
+++ app.py
@@ -14,52 +14,52 @@ def draw_page():
         tma_col, sla_col, inicio_col, fim_col, upload_col = st.columns([1,1,1,1,1])
         
         with tma_col:
-            tma = st.number_input(label="TMA - Segundos", value=156, step=1, key="tma_input")
+            st.session_state.tma = st.number_input(label="TMA - Segundos", value=156, step=1, key="tma_input")
             
         with sla_col:
-            sla = st.number_input(label="SLA - Minutos", step=1, key="sla_input")
+            st.session_state.sla = st.number_input(label="SLA - Minutos", step=1, key="sla_input")
             
         with inicio_col:
-            inicio_op = st.selectbox(label="Inicio Operacao", options=["07:00", "08:00"], key="inicio_op_input")
+            st.session_state.inicio_op = st.selectbox(label="Inicio Operacao", options=["07:00", "08:00"], key="inicio_op_input")
             
         with fim_col:
-            fim_op = st.selectbox(label="Final Operacao", options=["22:00", "21:00"], key="fim_op_input")
+            st.session_state.fim_op = st.selectbox(label="Final Operacao", options=["22:00", "21:00"], key="fim_op_input")
             
         with upload_col:
             uploaded_file = st.file_uploader(label="CSV")
-            time_range = Data_Man.get_range(inicio_op, fim_op)
+            st.session_state.time_range = Data_Man.get_range(st.session_state.inicio_op, st.session_state.fim_op)
             
             if uploaded_file is None:
-                df_derivacao = Data_Man.get_dataframe_vazio()
-                df_producao = Data_Man.get_dataframe_vazio()
-                df_acumulo = Data_Man.get_dataframe_vazio()
+                st.session_state.df_derivacao = Data_Man.get_dataframe_vazio()
+                st.session_state.df_producao = Data_Man.get_dataframe_vazio()
+                st.session_state.df_acumulo = Data_Man.get_dataframe_vazio()
                 
             else:
                 # if not athena:
-                df_csv = Data_Man.get_dataframe(uploaded_file)                
-                calculadora = Calculadora()
+                st.session_state.df_csv = Data_Man.get_dataframe(uploaded_file)                
+                st.session_state.calculadora = Calculadora()
                                 
-                (demanda_atual, 
-                 demanda_acumulada, 
-                 capacidade_operacional) = calculadora.create_instancias()
+                (st.session_state.demanda_atual, 
+                 st.session_state.demanda_acumulada, 
+                 st.session_state.capacidade_operacional) = st.session_state.calculadora.create_instancias()
                 
-                demanda_inicial = df_csv['quantidade'].tolist()
-                demanda_atual.set_demanda(demanda_inicial)
+                st.session_state.demanda_inicial = st.session_state.df_csv['quantidade'].tolist()
+                st.session_state.demanda_atual.set_demanda(st.session_state.demanda_inicial)
                 
-                acumulo_inicial = calculadora.calcular_acumulo(demanda_inicial)
-                demanda_acumulada.set_demanda(acumulo_inicial)
+                st.session_state.acumulo_inicial = st.session_state.calculadora.calcular_acumulo(st.session_state.demanda_inicial)
+                st.session_state.demanda_acumulada.set_demanda(st.session_state.acumulo_inicial)
                 
-                analistas_lista = calculadora.athena(
-                    tma, 
-                    demanda_atual, 
-                    demanda_acumulada, 
-                    capacidade_operacional
+                st.session_state.analistas_lista = st.session_state.calculadora.athena(
+                    st.session_state.tma, 
+                    st.session_state.demanda_atual, 
+                    st.session_state.demanda_acumulada, 
+                    st.session_state.capacidade_operacional
                 )
 
                 #Criação dos dataframes para a plotagem dos graficos
-                df_derivacao = df_csv 
-                df_producao = Data_Man.get_custom_dataframe(capacidade_operacional.get_capacidade_producao())
-                df_acumulo = Data_Man.get_custom_dataframe(demanda_acumulada.get_demanda())
+                st.session_state.df_derivacao = st.session_state.df_csv 
+                st.session_state.df_producao = Data_Man.get_custom_dataframe(st.session_state.capacidade_operacional.get_capacidade_producao())
+                st.session_state.df_acumulo = Data_Man.get_custom_dataframe(st.session_state.demanda_acumulada.get_demanda())
             
                 # if athena
                 
@@ -75,57 +75,37 @@ def draw_page():
             der_cap_col, acum_col = st.columns([1,1])
             
             with der_cap_col:
-                Graficos.draw_grafico_demanda_capacidade(
-                    df_derivacao, 
-                    'skyblue', 
-                    df_producao, 
-                    'violet', 
-                    time_range, 
-                    'du_cap_vazio'
-                )
-            
+                Graficos.draw_grafico_demanda_capacidade('du_cap_vazio')
+
             with acum_col:
-                Graficos.draw_grafico(df_acumulo,'salmon', time_range, 'du_acum_vazio')
+                Graficos.draw_grafico('du_acum_vazio')
                                 
         with sab_tab:
             der_cap_col, acum_col = st.columns([1,1])
             
             with der_cap_col:
-                Graficos.draw_grafico_demanda_capacidade(
-                    df_derivacao, 
-                    'skyblue', 
-                    df_producao, 
-                    'violet', 
-                    time_range,
-                    'sab_cap_vazio'
-                )
+                Graficos.draw_grafico_demanda_capacidade('sab_cap_vazio')
             
             with acum_col:
-                Graficos.draw_grafico(df_acumulo,'salmon', time_range, 'sab_acum_vazio')
+                Graficos.draw_grafico('sab_acum_vazio')
                 
             
         with dom_tab:
             der_cap_col, acum_col = st.columns([1,1])
             
             with der_cap_col:
-                Graficos.draw_grafico_demanda_capacidade(
-                    df_derivacao, 
-                    'skyblue', 
-                    df_producao, 
-                    'violet', 
-                    time_range,
-                    'dom_cap'
-                )
+                Graficos.draw_grafico_demanda_capacidade('dom_cap')
 
             with acum_col:
-                Graficos.draw_grafico(df_acumulo,'salmon', time_range, 'dom_acum')
+                Graficos.draw_grafico('dom_acum')
         
     with st.container(): # Tabela
-        st.caption(f"Analistas - {len(analistas_lista)}")
         if uploaded_file is None:
+            st.caption(f"Analistas - {0}")
             pass
         else:
-            analistas_agrupados = Data_Man.get_analistas_agrupados(analistas_lista)
+            st.caption(f"Analistas - {len(st.session_state.analistas_lista)}")
+            analistas_agrupados = Data_Man.get_analistas_agrupados(st.session_state.analistas_lista)
             
             for analista, row in analistas_agrupados.iterrows():
                 with st.container():
@@ -140,15 +120,15 @@ def draw_page():
                         
                     with entrada_col:
                         st.caption("Entrada")
-                        st.write(f"{analistas_lista[analista].get_horarios()[0]}")
+                        st.write(f"{st.session_state.analistas_lista[analista].get_horarios()[0]}")
                         
                     with almoco_col:
                         st.caption("Almoco")
-                        st.write(f"{analistas_lista[analista].get_horarios()[1]}")
+                        st.write(f"{st.session_state.analistas_lista[analista].get_horarios()[1]}")
                         
                     with saida_col:
                         st.caption("Saida")
-                        st.write(f"{analistas_lista[analista].get_horarios()[2]}")
+                        st.write(f"{st.session_state.analistas_lista[analista].get_horarios()[2]}")
                 st.divider()
 
 def main():
